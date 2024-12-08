beab81635e1b97dd496088ef33ab0d40c591341d
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2024-12-08 07:47:14 +0000

    feat:Added the main class

---
+++ src/main/java/com/example/productmanagementsys/ProductmanagementsysApplication.java
@@ -0,0 +1,13 @@
+package com.example.productmanagementsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmanagementsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmanagementsysApplication.class, args);
+	}
+
+}
