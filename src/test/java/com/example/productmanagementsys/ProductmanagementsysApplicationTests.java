5dcc4069426a66b6e5243a34f8422d688a653343
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2024-12-08 07:49:52 +0000

    feat: Added the Test directory

---
+++ src/test/java/com/example/productmanagementsys/ProductmanagementsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productmanagementsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmanagementsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
