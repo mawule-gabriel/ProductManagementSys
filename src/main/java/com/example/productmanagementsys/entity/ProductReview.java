2bfaa309e1e1ea55ecb4df7423b106a2b076c8aa
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2024-12-19 13:35:41 +0000

    Update product review class to handle ratings 1-5

--- src/main/java/com/example/productmanagementsys/entity/ProductReview.java
+++ src/main/java/com/example/productmanagementsys/entity/ProductReview.java
@@ -4,6 +4,8 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.time.LocalDateTime;
 
 @Data
@@ -18,6 +20,8 @@ public class ProductReview {
     private String productId;   // The product being reviewed
     private String username;     // The user who wrote the review
     private String content;      // Review content
+
+    @Min(1) @Max(5) // Rating between 1 and 5
     private int rating;          // Rating for the product (1-5)
     private LocalDateTime timestamp; // Timestamp of when the review was created
 }
